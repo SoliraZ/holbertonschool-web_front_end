8e0adc355370cf5dc71f635e74c819a108713875
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-02-02 04:06:21 +0100

    Add changeMode function in 5-mode.js to dynamically alter webpage styles. This implementation includes multiple modes (spooky, dark, scream) triggered by buttons, enhancing user interaction and visual customization.

---
+++ Javascript_advanced/5-mode.js
@@ -0,0 +1,38 @@
+function changeMode(size, weight, transform, background, color) {
+    return function () {
+        document.body.style.fontSize = size + 'px';
+        document.body.style.fontWeight = weight;
+        document.body.style.textTransform = transform;
+        document.body.style.backgroundColor = background;
+        document.body.style.color = color;
+    }
+}
+
+function main() {
+    let spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+    let darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
+    let screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
+
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = 'Welcome Holberton!';
+    document.body.appendChild(paragraph);
+
+    const buttonSpooky = document.createElement('button');
+    buttonSpooky.textContent = 'Spooky';
+    buttonSpooky.addEventListener('click', spooky);
+    document.body.appendChild(buttonSpooky);
+
+    const buttonDark = document.createElement('button');
+    buttonDark.textContent = 'Dark mode';
+    buttonDark.addEventListener('click', darkMode);
+    document.body.appendChild(buttonDark);
+
+    const buttonScream = document.createElement('button');
+    buttonScream.textContent = 'Scream mode';
+    buttonScream.addEventListener('click', screamMode);
+    document.body.appendChild(buttonScream);
+
+}
+
+main();
